1e657add0c77d8fd6e833c938f015ef1d1b2aa80
black <madimad@gmail.com>
2022-03-23 09:50:43 +0100

    add files

---
+++ form.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCm3pRvFPPDtqmUwnWfNqPYJVxx2PdHJX8",
+    authDomain: "monsupersite-cyberdev.firebaseapp.com",
+    databaseURL: "https://monsupersite-cyberdev-default-rtdb.europe-west1.firebasedatabase.app/",
+    projectId: "monsupersite-cyberdev",
+    storageBucket: "monsupersite-cyberdev.appspot.com",
+    messagingSenderId: "718540300731",
+    appId: "1:718540300731:web:8ecc2f08fc0887fe42249e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
